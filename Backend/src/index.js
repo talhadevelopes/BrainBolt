8a287fd2554502a2a65665d6f684a7365b43028d
Talha Ahmed <talha.codes970@gmail.com>
2025-06-14 20:19:45 +0530

    docker deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
@@ -8,24 +8,24 @@ const { ApifyClient } = require("apify-client");
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const  MainRouter   = require("./Routes/index");
-
+const MainRouter = require("./Routes/index");
 const UserModel = require("../models/UsersSchema");
 
 const MONGOOSE_URL = process.env.MONGO_URL;
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/api/v1", MainRouter);
+// Healthcheck endpoint
+app.get('/healthcheck', (req, res) => res.status(200).send('Healthy'));
 
+app.use("/api/v1", MainRouter);
 
 mongoose
   .connect(MONGOOSE_URL)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Something went wrong", err));
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.listen(3000, () => { 
   console.log("Server is running at port 3000");
